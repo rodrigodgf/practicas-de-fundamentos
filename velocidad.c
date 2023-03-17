c9c758364b63a614f0392ecaf30d2c9b2ccb6758
rodrigodgf <109828599+rodrigodgf@users.noreply.github.com>
2023-03-16 21:03:24 -0600

    Update velocidad.c

--- velocidad.c
+++ velocidad.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
-
+//ya funciona, creo
 float d1;
-float TSE=0 , VMS=0 , VKH=0 ; 
+float TSE=0 , VMS=0 , VKH=0;
+//float ;
+//float ; 
 int min ;
 int seg ;
-int cent ;
+int cent;
 
 
 int main () {
 
-    printf("ingresa tu distancia: ");
+    printf("ingresa tu distancia en metros: ");
     scanf ("%f" , &d1);
-
     printf ("escribe tu tiempo en min, seg, cent: ");
-    scanf ("%d , %d , %d" , min , seg , cent );
+    // por qué no funciona si pongo las tres en el mismo scanf?
+
+    scanf ("%d" , &min);
+    scanf("%d" , &seg);
+    scanf("%d" , &cent);
 
     TSE = (min*60)+seg+(cent/100);
     VMS = d1/TSE;
     VKH = (VMS*3600)/1000;
 
 
-    printf ("tu velocidad es de : %f" , VKH );
+    printf ("tu velocidad es de : %6.3f" , VKH );
 
 
 
